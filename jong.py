03cad1428cbca650af04b4853a41c9b6aa002162
김종호 <kj1351@naver.com>
2022-07-28 11:23:58 +0900

    7.28 3번째 수업 조건문 커밋!

--- jong.py
+++ jong.py
@@ -1,1 +1,11 @@
-print('a\nb\nc\nd')
+a = [5,20,35,10,8]
+if a[0]>a[1] and a[0]>a[2] and a[0]>a[3] and a[0]>a[4]:
+    print(a[0])
+elif a[1]>a[2] and a[1]>a[3] and a[1]>a[4]:
+    print(a[1])
+elif a[2]>a[3] and a[2]>a[4]:
+    print(a[2])
+elif a[3]>a[4]:
+    print(a[3])
+else:
+    print(a[4])
